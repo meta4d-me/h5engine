85f3a78398931e66d76e97a44237ea6e5cb54797
zhaodan <sagfa>
2017-05-26 11:24:06 +0800

    同步代码

--- gd3d/framework/particle/particletool/particles.ts
+++ gd3d/framework/particle/particletool/particles.ts
@@ -596,6 +596,10 @@ namespace gd3d.framework
                     gd3d.math.pool.delete_quaternion(lookRot);
                     return;
                 }
+                else if (this.renderModel == RenderModel.Mesh) 
+                {
+                        EffectUtil.quatLookatZ(worldTranslation, cameraTransform.getWorldTranslate(), worldRotation);
+                }
                 gd3d.math.quatMultiply(worldRotation, this.rotationByEuler, worldRotation);
                 //消除transform组件对粒子本身的影响
                 gd3d.math.quatClone(this.gameObject.transform.getWorldRotate(), invTransformRotation);
