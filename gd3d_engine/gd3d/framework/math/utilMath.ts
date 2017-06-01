d267de7f3c6c9abf560dd2f1fc6758261f5a445a
董波 <dongb@cafegame.cn>
2017-06-01 10:31:34 +0800

    1

--- gd3d_engine/gd3d/framework/math/utilMath.ts
+++ gd3d_engine/gd3d/framework/math/utilMath.ts
@@ -1,9 +1,7 @@
 //找不到归属的放这里
-namespace gd3d.math
-{
+namespace gd3d.math {
     //row：图片行数//column:图片列数//index：第几张图片（index从0开始计数）
-    export function spriteAnimation(row: number, column: number, index: number, out: vector4)
-    {
+    export function spriteAnimation(row: number, column: number, index: number, out: vector4) {
         var width = 1 / column;
         var height = 1 / row;
         var offsetx = width * (index % column);
@@ -16,4 +14,30 @@ namespace gd3d.math
         // var uvOffset=new gd3d.math.vector4(width,height,offsetx,offsety);
         // return  uvOffset;
     }
+
+    // export function GetPointAlongCurve(curveStart:vector3,curveStartHandle:vector3,curveEnd:vector3,curveEndHandle:vector3,t:number, out:vector3,crease:number=0.3)
+    // {
+    //     var oneMinT = 1 - t;
+    //     var oneMinTPow3 =Math.pow(oneMinT, 3);
+    //     var oneMinTPow2 = Math.pow(oneMinT, 2);
+
+    //     var oneMinCrease = 1 - crease;
+
+    //     var tempt1=gd3d.math.pool.new_vector3();
+    //     gd3d.math.vec3ScaleByNum(curveStart,oneMinTPow3*oneMinCrease,tempt1);
+    //     var tempt2=gd3d.math.pool.new_vector3();
+    //     gd3d.math.vec3ScaleByNum(curveStartHandle,3*oneMinTPow2*t*crease,tempt2);
+    //     var tempt3=gd3d.math.pool.new_vector3();
+    //     gd3d.math.vec3ScaleByNum(curveEndHandle,3*oneMinT*Math.pow(t,2)*crease,tempt3);
+    //     var tempt4=gd3d.math.pool.new_vector3();
+    //     gd3d.math.vec3ScaleByNum(curveEnd,Math.pow(t,3)*oneMinCrease,tempt4);
+
+    //     var tempt5=gd3d.math.pool.new_vector3();
+    //     gd3d.math.vec3Add(tempt1,tempt2,tempt5);
+    //     gd3d.math.vec3Add(tempt5,tempt3,tempt5);
+    //     gd3d.math.vec3Add(tempt5,tempt4,tempt5);
+        
+    //    // gd3d.math.vec3ScaleByNum(tempt5,1/(oneMinTPow3*oneMinCrease+));
+    
+    // }
 }
