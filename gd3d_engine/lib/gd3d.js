50a9a14075864c043a6ef0938774f860f3d585f1
xiaolong <xiaolongchase@163.com>
2017-06-12 14:06:43 +0800

    fix

--- gd3d_engine/lib/gd3d.js
+++ gd3d_engine/lib/gd3d.js
@@ -49,7 +49,7 @@ var gd3d;
             application.prototype.start = function (div) {
                 console.log("version: " + this.version + "  build: " + this.build);
                 framework.sceneMgr.app = this;
-                this.timeScale = 0.5;
+                this.timeScale = 1;
                 this.container = div;
                 var canvas = document.createElement("canvas");
                 canvas.className = "full";
