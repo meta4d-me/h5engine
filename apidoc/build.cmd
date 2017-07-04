09c4d6b5b48525ad85ceb22bf40ffa2f80b6a9fb
ming <95183008@qq.com>
2017-07-04 14:59:33 +0800

    apidoc

--- apidoc/build.cmd
+++ apidoc/build.cmd
@@ -1,1 +1,1 @@
-node ./apidocgenerater/api/tscdoc.js --path ../gd3d_engine/gd3d/ --output ./apidocdata/data/
+node ./apidocgenerater/api/tscdoc.js --path ../gd3d_engine/gd3d/ --output ./build/apidocdata/data/
