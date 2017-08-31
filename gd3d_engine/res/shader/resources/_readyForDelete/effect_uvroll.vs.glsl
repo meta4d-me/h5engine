2c92dd52911d6a13500be860507dc327435246a3
heziwen <w3r4333>
2017-08-31 12:49:39 +0800

    shader修改  properties 中暴露必要的 参数 （为了编辑器）

--- gd3d_engine/res/shader/resources/_readyForDelete/effect_uvroll.vs.glsl
+++ gd3d_engine/res/shader/resources/_readyForDelete/effect_uvroll.vs.glsl
@@ -1,7 +1,7 @@
 attribute vec4 _glesVertex;   
 attribute vec4 _glesColor;                  
 attribute vec4 _glesMultiTexCoord0;      
-uniform vec4 _mainColor;  
+uniform vec4 _MainColor;  
 uniform highp mat4 glstate_matrix_mvp;//这个uniform 是自动传递的      
 varying lowp vec4 xlv_COLOR;                
 varying highp vec2 xlv_TEXCOORD0;    
@@ -13,7 +13,7 @@ void main()
     highp vec4 tmpvar_1;                        
     tmpvar_1.w = 1.0;                           
     tmpvar_1.xyz = _glesVertex.xyz;             
-    xlv_COLOR = _glesColor + _mainColor;                     
+    xlv_COLOR = _glesColor + _MainColor;                     
     highp float step =  mod(glstate_timer , 2.0);
     xlv_TEXCOORD0 = _glesMultiTexCoord0.xy - vec2(step*_MainTex_STSpeed.x,step*_MainTex_STSpeed.y);   
     gl_Position = (glstate_matrix_mvp * tmpvar_1);  
