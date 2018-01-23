322142d8014b181c0face972c83a13c7238abc3f
heziwen <w3r4333>
2018-01-23 20:31:40 +0800

    transparent.shader.json 添加 贴图叠加 倍数 的字段

--- gd3d_engine/res/shader/resources/using/transparent.fs.glsl
+++ gd3d_engine/res/shader/resources/using/transparent.fs.glsl
@@ -1,6 +1,7 @@
 uniform lowp sampler2D _MainTex;                                                                                                
 varying highp vec2 xlv_TEXCOORD0;
 uniform lowp float _Alpha;
+uniform lowp float _Superimposition;
 
 #ifdef FOG
 uniform lowp vec4 glstate_fog_color; 
@@ -11,6 +12,7 @@ void main()
 {
     lowp vec4 emission = texture2D(_MainTex, xlv_TEXCOORD0);
     emission.a=emission.a*_Alpha;
+    emission.xyz *= _Superimposition;
     //----------------------------------------------------------
     #ifdef FOG
     emission= mix(vec4(0,0,0,0), emission, factor);
