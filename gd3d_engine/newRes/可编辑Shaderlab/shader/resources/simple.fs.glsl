9b3fb3907c7654940e42267e6e5a34439035e488
feng <908087098@qq.com>
2020-03-28 15:36:37 +0800

    调整 场景面片shader

--- gd3d_engine/newRes/可编辑Shaderlab/shader/resources/simple.fs.glsl
+++ gd3d_engine/newRes/可编辑Shaderlab/shader/resources/simple.fs.glsl
@@ -1,7 +1,6 @@
 precision highp float;
 
 uniform sampler2D _MainTex;
-uniform float _AlphaCut;
 uniform vec4 _MainColor;
 
 varying vec2 xlv_TEXCOORD0;    
@@ -14,9 +13,6 @@ varying lowp float factor;
 void main() 
 {
     vec4 basecolor = texture2D(_MainTex, xlv_TEXCOORD0);
-
-    if(basecolor.a < _AlphaCut)
-        discard;
 
     basecolor = basecolor * _MainColor;
 
