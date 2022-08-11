29c8e952eff98504296fdff107168591eb7bcbab
heziwen <273280521@qq.com>
2022-08-11 17:16:45 +0800

    粒子系统 fix

--- src/framework/asset/factorys/assetfactory_particlesystem.ts
+++ src/framework/asset/factorys/assetfactory_particlesystem.ts
@@ -2,7 +2,6 @@ namespace m4m.framework {
     @assetF(AssetTypeEnum.ParticleSystem)
     export class AssetFactory_ParticleSystem implements IAssetFactory {
         parse(assetmgr: assetMgr, bundle: assetBundle, name: string, txt: string) {
-            if (txt.indexOf(`gd3d.`) != -1) { txt = txt.replaceAll(`gd3d.`, `m4m.`); }     //xx的工具特效导出资源被植入了 code域名信息，暂时这样处理。
             var data = ParticleSystemData.get(name);
 
             if (!data) {
