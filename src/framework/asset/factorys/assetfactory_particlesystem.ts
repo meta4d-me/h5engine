85ad2da48f48c4f41e77adca90403cdca4f644a9
heziwen <273280521@qq.com>
2022-08-11 16:13:12 +0800

    ParticleSystem 资源内置 gd3d字段问题处理

--- src/framework/asset/factorys/assetfactory_particlesystem.ts
+++ src/framework/asset/factorys/assetfactory_particlesystem.ts
@@ -1,15 +1,11 @@
-namespace m4m.framework
-{
+namespace m4m.framework {
     @assetF(AssetTypeEnum.ParticleSystem)
-    export class AssetFactory_ParticleSystem implements IAssetFactory
-    {
-        parse(assetmgr: assetMgr, bundle: assetBundle, name: string, txt: string)
-        {
-
+    export class AssetFactory_ParticleSystem implements IAssetFactory {
+        parse(assetmgr: assetMgr, bundle: assetBundle, name: string, txt: string) {
+            if (txt.indexOf(`gd3d.`) != -1) { txt = txt.replaceAll(`gd3d.`, `m4m.`); }     //xx的工具特效导出资源被植入了 code域名信息，暂时这样处理。
             var data = ParticleSystemData.get(name);
 
-            if (!data)
-            {
+            if (!data) {
                 data = new ParticleSystemData();
                 data.value = name;
             }
