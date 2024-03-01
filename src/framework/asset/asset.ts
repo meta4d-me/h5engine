ca3fdcc838c09af05d78566318fd74bd62418024
MetaYoyo <olymyoyo@gmail.com>
2024-03-01 16:18:02 +0800

    clean code, add license to publish

--- src/framework/asset/asset.ts
+++ src/framework/asset/asset.ts
@@ -1,3 +1,19 @@
+/**
+@license
+Copyright 2022 meta4d.me Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
 namespace m4m.framework {
     //资源不能随意改名，否则药丸
     //资源需要有一个固定的名字，一个唯一的id
